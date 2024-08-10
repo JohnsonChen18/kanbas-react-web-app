13a5369ec23e8f0f37625a1b19c1b1958a0f3d77
MiaozhiChen <chen.miaoz@northeastern.edu>
2024-08-10 18:37:21 -0400

    add names on the landing page

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -12,22 +12,24 @@ import Lab6 from "./Lab6";
 export default function Labs() {
     return (
         <Provider store={store}>
-        <div>
-            <h1>Name: Miaozhi Chen (Section 02)</h1><br/>
-            {/*<a href="https://github.com/JohnsonChen18/kanbas-react-web-app.git" target="_blank">My github</a>*/}
-            <br/> <br/>
-            <h1>Labs</h1>
-            <TOC/>
-            <Routes>
-                <Route path="/" element={<Navigate to="Lab1"/>}/>
-                <Route path="Lab1" element={<Lab1/>}/>
-                <Route path="Lab2" element={<Lab2/>}/>
-                <Route path="Lab3/*" element={<Lab3/>}/>
-                <Route path="Lab4/*" element={<Lab4/>}/>
-                <Route path="Lab5/*" element={<Lab5/>}/>
-                <Route path="Lab6/*" element={<Lab6/>}/>
-            </Routes>
-        </div>
+            <div>
+                <h1>Web Dev Team 15</h1><br/>
+                <h1>Name: Miaozhi Chen (Section 02)</h1><br/>
+                <h1>Name: Jiarong Zhu(Section 02)</h1><br/>
+                {/*<a href="https://github.com/JohnsonChen18/kanbas-react-web-app.git" target="_blank">My github</a>*/}
+                <br/> <br/>
+                <h1>Labs</h1>
+                <TOC/>
+                <Routes>
+                    <Route path="/" element={<Navigate to="Lab1"/>}/>
+                    <Route path="Lab1" element={<Lab1/>}/>
+                    <Route path="Lab2" element={<Lab2/>}/>
+                    <Route path="Lab3/*" element={<Lab3/>}/>
+                    <Route path="Lab4/*" element={<Lab4/>}/>
+                    <Route path="Lab5/*" element={<Lab5/>}/>
+                    <Route path="Lab6/*" element={<Lab6/>}/>
+                </Routes>
+            </div>
         </Provider>
     );
 }
